a830f1074d3b192e819e128bce43fb7044211bd0
Bharath Kumar <bharath7242@gmail.com>
2017-11-24 16:24:19 +0530

    Update reponse.js

--- reponse.js
+++ reponse.js
@@ -15,6 +15,380 @@ var data = {
         "uncalled_tasks": 2
       },
       "team_leaders": [
+         {
+          "info": {
+            "id": 25,
+            "name": "Harry Potter",
+            "dp_url": "http://www.example.com/harry37409.jpg",
+            "abs": {
+              "calls": 10,
+              "tdrives": 12,
+              "visits": 3,
+              "post_bookings": 8,
+              "deliveries": 5,
+              "uncalled_tasks": 2
+            }
+          },
+          "sales_consultants": [
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            },
+             {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            },
+             {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            },
+             {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            },
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            }
+          ]
+        },
+         {
+          "info": {
+            "id": 25,
+            "name": "Harry Potter",
+            "dp_url": "http://www.example.com/harry37409.jpg",
+            "abs": {
+              "calls": 10,
+              "tdrives": 12,
+              "visits": 3,
+              "post_bookings": 8,
+              "deliveries": 5,
+              "uncalled_tasks": 2
+            }
+          },
+          "sales_consultants": [
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            },
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            }
+          ]
+        },
+         {
+          "info": {
+            "id": 25,
+            "name": "Harry Potter",
+            "dp_url": "http://www.example.com/harry37409.jpg",
+            "abs": {
+              "calls": 10,
+              "tdrives": 12,
+              "visits": 3,
+              "post_bookings": 8,
+              "deliveries": 5,
+              "uncalled_tasks": 2
+            }
+          },
+          "sales_consultants": [
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            },
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            }
+          ]
+        },
+         {
+          "info": {
+            "id": 25,
+            "name": "Harry Potter",
+            "dp_url": "http://www.example.com/harry37409.jpg",
+            "abs": {
+              "calls": 10,
+              "tdrives": 12,
+              "visits": 3,
+              "post_bookings": 8,
+              "deliveries": 5,
+              "uncalled_tasks": 2
+            }
+          },
+          "sales_consultants": [
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            },
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            }
+          ]
+        },
+         {
+          "info": {
+            "id": 25,
+            "name": "Harry Potter",
+            "dp_url": "http://www.example.com/harry37409.jpg",
+            "abs": {
+              "calls": 10,
+              "tdrives": 12,
+              "visits": 3,
+              "post_bookings": 8,
+              "deliveries": 5,
+              "uncalled_tasks": 2
+            }
+          },
+          "sales_consultants": [
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            },
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            }
+          ]
+        },
+         {
+          "info": {
+            "id": 25,
+            "name": "Harry Potter",
+            "dp_url": "http://www.example.com/harry37409.jpg",
+            "abs": {
+              "calls": 10,
+              "tdrives": 12,
+              "visits": 3,
+              "post_bookings": 8,
+              "deliveries": 5,
+              "uncalled_tasks": 2
+            }
+          },
+          "sales_consultants": [
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            },
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            }
+          ]
+        },
+         {
+          "info": {
+            "id": 25,
+            "name": "Harry Potter",
+            "dp_url": "http://www.example.com/harry37409.jpg",
+            "abs": {
+              "calls": 10,
+              "tdrives": 12,
+              "visits": 3,
+              "post_bookings": 8,
+              "deliveries": 5,
+              "uncalled_tasks": 2
+            }
+          },
+          "sales_consultants": [
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            },
+            {
+              "info": {
+                "id": 45,
+                "name": "Hermione Granger",
+                "dp_url": "http://www.example.com/hermione37409.jpg",
+                "abs": {
+                  "calls": 10,
+                  "tdrives": 12,
+                  "visits": 3,
+                  "post_bookings": 8,
+                  "deliveries": 5,
+                  "uncalled_tasks": 2
+                }
+              }
+            }
+          ]
+        },
         {
           "info": {
             "id": 25,
