a83ba4b5dbbba82a21bff06b593f942f1d62a89b
Bharath Kumar <bharath7242@gmail.com>
2017-11-24 17:01:52 +0530

    Update reponse.js

--- reponse.js
+++ reponse.js
@@ -110,7 +110,7 @@ var data = {
          {
           "info": {
             "id": 25,
-            "name": "Potter",
+            "name": "Oer",
             "dp_url": "http://www.example.com/harry37409.jpg",
             "abs": {
               "calls": 10,
