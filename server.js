7bb2cd26b670c6568cbe978ee1032972a951012e
Bharath Kumar <bharath.kumar@autoninja.in>
2017-11-24 13:27:16 +0530

    today summary created

--- server.js
+++ server.js
@@ -69,9 +69,58 @@ app.get('/bad', (req,res) => {
 
 app.get('/salescrm', (req,res) => {
   res.send({
-    errorMessage:'Unable handle the request',
-    code :'PAGE_NOT_FOUND'
-  });
+	"error" : {},
+	"message" : "",
+  "statusCode" : 2002,
+	"result" : [
+		{
+			"location_id" : 23,
+			"location_name" : "SP Road",
+			"location_abs" : {
+				"calls" : 10,
+				"tdrives" : 12,
+				"visits" : 3,
+				"post_bookings" : 8,
+				"deliveries" : 5,
+				"uncalled_tasks" : 2
+			},
+			"team_leaders" : [
+				{
+					"info" : {
+						"id" : 25,
+						"name" : "Harry Potter",
+						"dp_url" : "http://www.example.com/harry37409.jpg",
+						"abs" : {
+							"calls" : 10,
+							"tdrives" : 12,
+							"visits" : 3,
+							"post_bookings" : 8,
+							"deliveries" : 5,
+							"uncalled_tasks" : 2
+						}
+					},
+					"sales_consultants" : [
+						{
+							"info" : {
+								"id" : 45,
+								"name" : "Hermione Granger",
+								"dp_url" : "http://www.example.com/hermione37409.jpg",
+								"abs" : {
+									"calls" : 10,
+									"tdrives" : 12,
+									"visits" : 3,
+									"post_bookings" : 8,
+									"deliveries" : 5,
+									"uncalled_tasks" : 2
+								}
+							}
+						}
+					]
+				}
+			]
+		}
+	]
+});
 });
 
 
