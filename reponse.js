b4f8406de57c76946b0a9266edd4dd31f2442706
Bharath Kumar <bharath7242@gmail.com>
2017-11-29 18:34:00 +0530

    Update reponse.js

--- reponse.js
+++ reponse.js
@@ -482,52 +482,6 @@ var data = {"error": {},
           ]
         }
       ]
-    },
-    {
-      "location_id" : 23,
-      "location_name" : "Mysore Road",
-      "location_abs" : {
-        "calls" : 10,
-        "tdrives" : 12,
-        "visits" : 3,
-        "post_bookings" : 8,
-        "deliveries" : 5,
-        "uncalled_tasks" : 2
-      },
-      "team_leaders" : [
-        {
-          "info" : {
-            "id" : 25,
-            "name" : "Raajiv",
-            "dp_url" : "http://www.example.com/harry37409.jpg",
-            "abs" : {
-              "calls" : 10,
-              "tdrives" : 12,
-              "visits" : 3,
-              "post_bookings" : 8,
-              "deliveries" : 5,
-              "uncalled_tasks" : 2
-            }
-          },
-          "sales_consultants" : [
-            {
-              "info" : {
-                "id" : 45,
-                "name" : "Hermione Granger",
-                "dp_url" : "http://www.example.com/hermione37409.jpg",
-                "abs" : {
-                  "calls" : 10,
-                  "tdrives" : 12,
-                  "visits" : 3,
-                  "post_bookings" : 8,
-                  "deliveries" : 5,
-                  "uncalled_tasks" : 2
-                }
-              }
-            }
-          ]
-        }
-      ]
     }
   ]
 };
