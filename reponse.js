e6256351f88ba02329fae68d77c94975c5576d46
Bharath Kumar <bharath7242@gmail.com>
2018-06-29 15:42:25 +0530

    Update reponse.js

--- reponse.js
+++ reponse.js
@@ -1,3 +1,68 @@
-var data = {"error":{"type":"","details":""},"message":"","result":{"category_points":[{"id":3,"name":"Test Drive","points":[{"value":23,"count":3,"total":69}],"sum":69},{"id":2,"name":"Visit","points":[{"value":40,"count":1,"total":40}],"sum":40},{"id":1,"name":"Retail","points":[{"value":143,"count":1,"total":143}],"sum":143},{"id":5,"name":"Bonus","points":[{"value":3,"count":1,"total":3}],"sum":3}],"total":255},"statusCode":2002};
-        
+var data = {
+  "error": {
+    "type": "",
+    "details": ""
+  },
+  "message": "",
+  "result": {
+    "category_points": [
+      {
+        "id": 3,
+        "name": "Test Drive",
+        "points": [
+          {
+            "value": 23,
+            "count": 3,
+            "total": 69
+          },
+          {
+            "value": 20,
+            "count": 3,
+            "total": 60
+          }
+        ],
+        "sum": 129
+      },
+      {
+        "id": 2,
+        "name": "Visit",
+        "points": [
+          {
+            "value": 40,
+            "count": 1,
+            "total": 40
+          }
+        ],
+        "sum": 40
+      },
+      {
+        "id": 1,
+        "name": "Retail",
+        "points": [
+          {
+            "value": 143,
+            "count": 1,
+            "total": 143
+          }
+        ],
+        "sum": 143
+      },
+      {
+        "id": 5,
+        "name": "Bonus",
+        "points": [
+          {
+            "value": 3,
+            "count": 1,
+            "total": 3
+          }
+        ],
+        "sum": 3
+      }
+    ],
+    "total": 255
+  },
+  "statusCode": 2002
+};
+
 module.exports = {data};
