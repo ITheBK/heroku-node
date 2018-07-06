1378da2d9bd4c2839d4311b70fde58e4df369f13
Bharath Kumar <bharath7242@gmail.com>
2018-07-06 13:31:58 +0530

    Update reponse.js

--- reponse.js
+++ reponse.js
@@ -1,5 +1,96 @@
 var data = 
-{"error":{"type":"","details":""},"message":"","result":{"best_consultant":{"title":"Best Consultant","month":"June","best_dses":[{"name":"D SAI KUMAR","category":"Explorers","category_id":1,"photo_url":"https:\/\/s3-ap-southeast-1.amazonaws.com\/salescrm-images\/staging\/uploads\/profile_images\/talwar\/D SAI KUMAR-6-1530785932.png"},{"name":"MACHA RAMESH","category":"Dynamos","category_id":3,"photo_url":"https:\/\/s3-ap-southeast-1.amazonaws.com\/salescrm-images\/staging\/uploads\/profile_images\/talwar\/MACHA RAMESH-89-1514873182.png"},{"name":"VIKRANT TRIPATHY","category":"Acheivers","category_id":2,"photo_url":"https:\/\/s3-ap-southeast-1.amazonaws.com\/salescrm-images\/staging\/uploads\/profile_images\/talwar\/VIKRANT TRIPATHY-202-1522257955.png"}]},"best_team":{"team_id":56,"team_name":"Abdul Saheed","team_leader_id":207,"team_leader_name":"ABDUL SAHEED","team_leader_photo_url":"Abdul Saheed-207-1525257402.png","title":"Best Team","month":"June","team_users":[{"user_id":6,"user_name":"D SAI KUMAR","user_photo_url":"https:\/\/s3-ap-southeast-1.amazonaws.com\/salescrm-images\/staging\/uploads\/profile_images\/talwar\/D SAI KUMAR-6-1530785932.png"},{"user_id":177,"user_name":"M RAKESH","user_photo_url":"https:\/\/s3-ap-southeast-1.amazonaws.com\/salescrm-images\/staging\/uploads\/profile_images\/talwar\/M RAKESH-177-1510285613.png"},{"user_id":207,"user_name":"ABDUL SAHEED","user_photo_url":"https:\/\/s3-ap-southeast-1.amazonaws.com\/salescrm-images\/staging\/uploads\/profile_images\/talwar\/Abdul Saheed-207-1525257402.png"},{"user_id":215,"user_name":"Gaddam Shiva Shankar","user_photo_url":"https:\/\/s3-ap-southeast-1.amazonaws.com\/salescrm-images\/staging\/uploads\/profile_images\/talwar\/Gaddam Shiva Shankar-215-1524807491.png"},{"user_id":228,"user_name":"Kamsani Ramesh","user_photo_url":"https:\/\/s3-ap-southeast-1.amazonaws.com\/salescrm-images\/staging\/uploads\/profile_images\/talwar\/Kamsani Ramesh-228-1528263120.png"},{"user_id":229,"user_name":"P Srinivas Reddy","user_photo_url":"https:\/\/s3-ap-southeast-1.amazonaws.com\/salescrm-images\/staging\/uploads\/profile_images\/talwar\/P Srinivas Reddy-229-1530796686.png"}]}},"statusCode":2002}
-;
+{
+    "error": {
+        "type": "",
+        "details": ""
+    },
+    "message": "",
+    "result": {
+        "best_consultant": {
+            "title": "Best Consultant",
+            "month": "June",
+            "best_dses": [
+                {
+                    "name": "D SAI KUMAR",
+                    "category": "Explorers",
+                    "category_id": 1,
+                    "photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/D SAI KUMAR-6-1530785932.png"
+                },
+                {
+                    "name": "MACHA RAMESH",
+                    "category": "Dynamos",
+                    "category_id": 3,
+                    "photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/MACHA RAMESH-89-1514873182.png"
+                },
+                {
+                    "name": "VIKRANT TRIPATHY",
+                    "category": "Acheivers",
+                    "category_id": 2,
+                    "photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/VIKRANT TRIPATHY-202-1522257955.png"
+                }
+            ]
+        },
+        "best_team": {
+            "team_id": 56,
+            "team_name": "Abdul Saheed",
+            "team_leader_id": 207,
+            "team_leader_name": "ABDUL SAHEED",
+            "team_leader_photo_url": "Abdul Saheed-207-1525257402.png",
+            "title": "Best Team",
+            "month": "June",
+            "team_users": [
+                {
+                    "user_id": 6,
+                    "user_name": "D SAI KUMAR",
+                    "user_photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/D SAI KUMAR-6-1530785932.png"
+                },
+                {
+                    "user_id": 177,
+                    "user_name": "M RAKESH",
+                    "user_photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/M RAKESH-177-1510285613.png"
+                },
+                {
+                    "user_id": 207,
+                    "user_name": "ABDUL SAHEED",
+                    "user_photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/Abdul Saheed-207-1525257402.png"
+                },
+                {
+                    "user_id": 215,
+                    "user_name": "Gaddam Shiva Shankar",
+                    "user_photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/Gaddam Shiva Shankar-215-1524807491.png"
+                },
+                {
+                    "user_id": 228,
+                    "user_name": "Kamsani Ramesh",
+                    "user_photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/Kamsani Ramesh-228-1528263120.png"
+                },
+                {
+                    "user_id": 229,
+                    "user_name": "P Srinivas Reddy",
+                    "user_photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/P Srinivas Reddy-229-1530796686.png"
+                },
+                {
+                    "user_id": 229,
+                    "user_name": "P Srinivas Reddy",
+                    "user_photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/P Srinivas Reddy-229-1530796686.png"
+                }
+                ,
+                {
+                    "user_id": 229,
+                    "user_name": "P Srinivas Reddy",
+                    "user_photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/P Srinivas Reddy-229-1530796686.png"
+                }
+                ,
+                {
+                    "user_id": 229,
+                    "user_name": "P Srinivas Reddy",
+                    "user_photo_url": "https://s3-ap-southeast-1.amazonaws.com/salescrm-images/staging/uploads/profile_images/talwar/P Srinivas Reddy-229-1530796686.png"
+                }
+            ]
+        }
+    },
+    "statusCode": 2002
+};
+     
 
 module.exports = {data};
