94fdeb35b489db33064a05cafa93d1ca52a4e55b
Bharath Kumar <bharath7242@gmail.com>
2020-09-22 16:09:34 +0530

    Create ninjacrm-android.js

---
+++ ninjacrm-android.js
@@ -0,0 +1,167 @@
+let data = [
+    {
+        "prevCallData": [
+            {
+                "disposition": "Confirmed Booking",
+                "subDisposition": "Dse Home Visit",
+                "callType": "REMINDER",
+                "remarks": "Customer Confirmed the Booking",
+                "callDate": "2020-09-15 14:30:00"
+            },
+            {
+                "disposition": "Booking",
+                "subDisposition": "Dse Home Visit",
+                "callType": "FRESH",
+                "remarks": "Booking for DSE",
+                "callDate": "2020-09-10 09:30:30"
+            }
+        ],
+        "tagData": [
+            {
+                "name": "leadStatus",
+                "categoryName": "Lead Status",
+                "editable": true,
+                "multiSelect": false,
+                "tagValues": [
+                    {
+                        "name": "HOT",
+                        "value": "HOT"
+                    },
+                    {
+                        "name": "WARM",
+                        "value": "WARM"
+                    },
+                    {
+                        "name": "COLD",
+                        "value": "COLD"
+                    }
+                ]
+            }
+        ],
+        "vehicleProperties": {
+            "fuelType": "PETROL",
+            "model": "BALENO",
+            "vehicleProperty": "OWN",
+            "regNum": "KA12AB1234"
+        },
+        "callInfo": {
+            "callId": 1,
+            "status": "ACTIVE",
+            "callType": "DSE HOME VISIT",
+            "callDate": "2020-09-16 11:30:00"
+        },
+        "leadInfo": {
+            "customerCategory": "Own Customer",
+            "Location": "HSR Layout",
+            "renewalType": "3"
+        },
+        "customerInfo": {
+            "customerName": "Suneel",
+            "address": "Krishna vilas sowdham , Near Rotary Auditorium , Nuzvid",
+            "pickupAddress": "",
+            "contactInfo": [
+                {
+                    "mid": 1,
+                    "number": "7709736537"
+                }
+            ]
+        },
+        "policyDetails": {
+            "policyNum": "",
+            "expiryDate": "",
+            "ncb": "40%",
+            "premium": "17500",
+            "idv": "380000"
+        },
+        "prevPolicyDetails": {
+            "policyNum": "18001022355",
+            "expiryDate": "2020-09-20",
+            "ncb": "30%",
+            "premium": "15500",
+            "idv": "400000"
+        }
+    },
+    {
+        "prevCallData": [
+            {
+                "disposition": "Confirmed Booking",
+                "subDisposition": "Dse Home Visit",
+                "callType": "REMINDER",
+                "remarks": "Customer Confirmed the Booking",
+                "callDate": "2020-09-15 14:30:00"
+            },
+            {
+                "disposition": "Booking",
+                "subDisposition": "Dse Home Visit",
+                "callType": "FRESH",
+                "remarks": "Booking for DSE",
+                "callDate": "2020-09-10 09:30:30"
+            }
+        ],
+        "tagData": [
+            {
+                "name": "leadStatus",
+                "categoryName": "Lead Status",
+                "editable": true,
+                "multiSelect": false,
+                "tagValues": [
+                    {
+                        "name": "HOT",
+                        "value": "HOT"
+                    },
+                    {
+                        "name": "WARM",
+                        "value": "WARM"
+                    },
+                    {
+                        "name": "COLD",
+                        "value": "COLD"
+                    }
+                ]
+            }
+        ],
+        "vehicleProperties": {
+            "fuelType": "PETROL",
+            "model": "BALENO",
+            "vehicleProperty": "OWN",
+            "regNum": "KA12AB1234"
+        },
+        "callInfo": {
+            "callId": 1,
+            "status": "ACTIVE",
+            "callType": "DSE HOME VISIT",
+            "callDate": "2020-09-16 11:30:00"
+        },
+        "leadInfo": {
+            "customerCategory": "Own Customer",
+            "Location": "HSR Layout",
+            "renewalType": "3"
+        },
+        "customerInfo": {
+            "customerName": "Suneel",
+            "address": "Krishna vilas sowdham , Near Rotary Auditorium , Nuzvid",
+            "pickupAddress": "",
+            "contactInfo": [
+                {
+                    "mid": 1,
+                    "number": "7709736537"
+                }
+            ]
+        },
+        "policyDetails": {
+            "policyNum": "",
+            "expiryDate": "",
+            "ncb": "40%",
+            "premium": "17500",
+            "idv": "380000"
+        },
+        "prevPolicyDetails": {
+            "policyNum": "18001022355",
+            "expiryDate": "2020-09-20",
+            "ncb": "30%",
+            "premium": "15500",
+            "idv": "400000"
+        }
+    }
+]
+module.exports = {data}
